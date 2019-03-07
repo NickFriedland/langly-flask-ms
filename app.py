002fd079758cba46e01b5a1a78cd6a46d1bfa755
Nick Friedland <nick.friedland@gmail.com>
2019-03-07 13:15:15 -0800

    Writing python route to handle Newspaper url to article parse request

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, jsonify, request
 import readability
+from newspaper import Article
 
 app = Flask(__name__)
+
+
+@app.route('/parse', methods=['POST'])
+def post_url():
+    """receives POST req from node server containing URL,
+    returns parsed article"""
+    url = request.json['url']
+    article = Article(url)
+    article.download()
+    article.parse()
+    # print('ARTICLE PARSED', article)
+    parsed = {
+        'title': article.title,
+        'authors': article.authors,
+        'text': article.text,
+        'publish_date': article.publish_date
+    }
+
+    return jsonify(parsed)
 
 
 @app.route('/readability', methods=['POST'])
