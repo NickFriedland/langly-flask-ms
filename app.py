76b1bd4eaffe38087067d920d926f6acd211f8b6
Nick Friedland <nick.friedland@gmail.com>
2019-04-19 10:53:56 -0700

    installed cors package for flask and added to flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import readability
 from newspaper import Article
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/parse', methods=['POST'])
